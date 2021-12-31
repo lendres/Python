5363c55f4ca3dc7c16a746b6eb67f9c37561ae21
Lance A. Endres <lendres@fifthrace.com>
2021-12-31 07:49:57 -0600

    Update BivariateAnalysis.py

--- lendres/BivariateAnalysis.py
+++ lendres/BivariateAnalysis.py
@@ -9,7 +9,7 @@ import seaborn as sns
 
 import lendres
 
-def CreateBiVariateHeatMap(data, scale=1.0):
+def CreateBiVariateHeatMap(data, scale=1.0, save=False):
     """
     Creates a new figure that has a bar plot labeled with a percentage for a single variable analysis.  Does this
     for every entry in the list of categories.
@@ -20,6 +20,8 @@ def CreateBiVariateHeatMap(data, scale=1.0):
         The data.
     scale : double
         Scaling parameter used to adjust the plot fonts, lineweights, et cetera for the output scale of the plot.
+    save : bool
+        If true, the plots are saved to the default plotting directory.
         
     Returns
     -------
@@ -30,17 +32,21 @@ def CreateBiVariateHeatMap(data, scale=1.0):
      # Must be run before creating figure or plotting data.
     lendres.Plotting.FormatPlot(scale=scale)
     
-    axis = sns.heatmap(data.corr(), annot=True)
+    axis = sns.heatmap(data.corr(), annot=True, annot_kws={"fontsize" : 10})
     axis.set(title="Heat Map for Continuous Data")
     
     figure = plt.gcf()
     
     plt.show()
-    
+
+    if save:
+        fileName = "Bivariante Heat Map"
+        lendres.Plotting.SavePlot(fileName, figure=figure, useDefaultOutputFolder=True)
+
     return figure
 
 
-def CreateBiVariatePairPlot(data, scale=1.0):
+def CreateBiVariatePairPlot(data, scale=1.0, save=False):
     """
     Creates a new figure that has a bar plot labeled with a percentage for a single variable analysis.  Does this
     for every entry in the list of categories.
@@ -51,6 +57,8 @@ def CreateBiVariatePairPlot(data, scale=1.0):
         The data.
     scale : double
         Scaling parameter used to adjust the plot fonts, lineweights, et cetera for the output scale of the plot.
+    save : bool
+        If true, the plots are saved to the default plotting directory.
         
     Returns
     -------
@@ -71,5 +79,10 @@ def CreateBiVariatePairPlot(data, scale=1.0):
     figure = plt.gcf()
     
     plt.show()
+    
+    if save:
+        fileName = "Bivariante Pair Plot"
+        lendres.Plotting.SavePlot(fileName, figure=figure, useDefaultOutputFolder=True)
+
     
     return figure
