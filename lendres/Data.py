77175a4403b8300da3e55640d1ef7945ae8b9a47
Lance A. Endres <lendres@fifthrace.com>
2022-02-03 21:20:29 -0600

    Data and linear regression updates

--- lendres/Data.py
+++ lendres/Data.py
@@ -6,6 +6,7 @@ Created on Mon Dec 27 18:06:31 2021
 """
 import pandas as pd
 import numpy as np
+import scipy.stats as stats
 from IPython.display import display
 
 import lendres
@@ -102,25 +103,474 @@ def ChangeToCategory(data, categories):
 
 def DropRowsWhereDataNotAvailable(data, category, inPlace=False):
     """
+    Drops any rows that do not have data available in the category of "category."
+    
     Parameters
     ----------
     data : pandas.DataFrame
         DataFrame to change the categories in.
     category : string
         Names of the category to look for not available entries.
+    inPlace : bool
+        If true, the modifications are done in place.
 
     Returns
     -------
     DataFrame without the removed rows or None if inPlace=True.
     """
     # Gets an DataSeries of boolean values indicating where values were not available.
-    notAvailableMask = data["Price"].isna()
+    indexMask = data[category].isna()
+    
+    # The indexMask is a DataSeries that has the indexes from the original DataFrame and the values are the result
+    # of the test statement (bools).  The indices do not necessarily correspond to the location in the DataFrame.  For
+    # example some rows may have been removed.  Extract only the indices we want to remove by using the mask itself.
+    dropIndices = indexMask[indexMask].index
+    
+    # Drop the rows.
+    return data.drop(dropIndices, inplace=inPlace)
+    
 
-    # numpy.where returns array inside of a tuple for some odd reason.  The [0] extracts the array.
-    dropIndices = np.where(notAvailableMask)[0]
+def DropAllRowsWhereDataNotAvailable(data, inPlace=False):
+    """
+    Drops any rows that are missing one or more entries from the data.
+    
+    Parameters
+    ----------
+    data : pandas.DataFrame
+        DataFrame to change the categories in.
+    inPlace : bool
+        If true, the modifications are done in place.
+
+    Returns
+    -------
+    DataFrame without the removed rows or None if inPlace=True.
+    """
+    # Gets an DataSeries of boolean values indicating where values were not available.
+    # to_numpy returns array inside of a tuple for some odd reason.  The [0] extracts the array.
+    dropIndices = data.isna().sum(axis=1).to_numpy().nonzero()[0]
 
     # Drop the rows.
-    if inPlace:
-        data.drop(dropIndices, inplace=inPlace)
+    return data.drop(dropIndices, inplace=inPlace)
+
+    
+def GetRowsWithMissingEntries(data):
+    """
+    Gets the rows that contain missing data.
+
+    Parameters
+    ----------
+    data : pandas.DataFrame
+        DataFrame to change the categories in.
+        
+    Returns
+    -------
+    A list that contains the indices of the rows that contain at least one missing entry.
+    """
+    
+    locations = data.isna().sum(axis=1).to_numpy().nonzero()[0]
+    count     = len(locations)
+        
+    return locations, count
+
+
+def ExtractLastStringTokens(data, categories):
+    """
+    Gets last string token from all the categories.
+
+    Parameters
+    ----------
+    data : pandas.DataFrame
+        DataFrame to change the categories in.
+    categories : list, array of strings
+        Names of the categories to operate on.
+
+    Returns
+    -------
+    A list that contains the indices of the rows that contain at least one missing entry.
+    """
+    # Initialize variables.
+    numberOfRows  = data.shape[0]
+    dataFrame     = pd.DataFrame(index=range(numberOfRows), columns=categories)
+
+    # Extract all the units information from the cells.  Loop over all the cells and extract the second half of the split string.
+    for category in categories:
+        for i in range(numberOfRows):
+            value = data[category].iloc[i]
+            dataFrame[category].iloc[i] = value.split()[1]
+    
+    return dataFrame
+
+
+def KeepFirstStringToken(value):
+    """
+    Takes a string that has multiple tokens and keeps only the first.
+
+    Parameters
+    ----------
+    value : string
+        The data entry.
+
+    Returns
+    -------
+    The first token of the string.
+    """
+    
+    # Make sure we are processing a string.
+    if isinstance(value, str):
+        # Splits the string at the space and returns the first entry as a number.
+        return value.split()[0]
+    
     else:
-        return data.drop(dropIndices)
+        # Entry wasn't a string, return an empty string.
+        return ""
+
+
+def KeepFirstTokenAsNumber(value):
+    """
+    Takes a string that has two tokens (a number followed by a string), extracts the numerical part, and returns it as a float.
+
+    Parameters
+    ----------
+    value : string
+        The data entry.
+
+    Returns
+    -------
+    float
+        The first token of the string as a number.
+    """
+    
+    # Make sure we are processing a string.
+    if isinstance(value, str):
+        # Splits the string at the space and returns the first entry as a number.
+        return float(value.split()[0])
+    
+    elif isinstance(value, float):
+        # Already a number, return it.
+        return value
+    
+    else:
+        # Entry wasn't a string or number, so return an out of range value.
+        return np.nan
+
+
+def ConvertMileage(value):
+    """
+    Takes a value from the mileage category, strips the units string, converts the units, and returns it as a float.
+
+    Parameters
+    ----------
+    value : string
+        The data entry.
+
+    Returns
+    -------
+    float
+        The mileage as km per liter.
+    """
+    
+    # Make sure we are processing a string.
+    if isinstance(value, str):
+        # Splits the string at the space and returns the first entry as a number.
+        splitString = value.split()
+        value       = float(splitString[0])
+        
+        if splitString[1] == "km/kg":
+            # Approximately 1.35 kg/liter.
+            # km   1.35 kg   km
+            # -- * ------  = --
+            # kg     l       l
+            value *= 1.35
+            
+        return value
+
+    elif isinstance(value, float):
+        # Already a number, return it.
+        return value
+    
+    else:
+        # Entry wasn't a string or number, so return an out of range value.
+        return np.nan
+
+
+def GetMinAndMaxValues(data, category, criteria, method="quantity"):
+    """
+    Display and maximum and minimum values in a DataSeries.
+    
+    Parameters
+    ----------
+    data : pandas.DataFrame
+        DataFrame to change the categories in.
+    category : string
+        Names of the category to sort and display.
+    criteria : list of two floats or a float
+        The criteria used to determine which numbers are dropped.  See "method" for more information.
+    method : string
+        How to determine which values are dropped.
+            percent - A percentage of the top and bottom values are dropped.
+            quantity - The number of values specified is dropped.
+
+    Returns
+    -------
+    pandas.DataFrame
+        A DataFrame that has both the minimum and maximum values, along with the indices where those values
+        occur.  The DataFrame contains the following headings:
+        Smallest_Index", "Smallest", "Largest_Index", "Largest"
+    """
+    # Initialize the variable so it is in scope.
+    numberOfRows = criteria
+    
+    if method == "quantity":
+        # Handled by the initialization above, no need to do anything except that the stupid ass Python parser
+        # thinks it needs something.
+        numberOfRows = criteria
+    elif method == "percent":
+        # Convert the fraction to a number of rows.
+        numberOfRows = round(len(data[category]) * criteria / 100)
+    else:
+        # A boo-boo was made.
+        raise Exception("Invalid \"method\" specified.")
+
+    # Sort then display the start and end of the series.
+    sortedSeries = data[category].sort_values()
+    
+    # Create new DataFrames for the head (smallest values) and the tail (largest values).
+    # Reset the index to move the index to a column and create a new, renumbered index.  This lets us combine the two DataFrames at
+    # the same index and saves the indices so we can use them later.
+    # Also rename the columns to make them more meaningful.
+    head = sortedSeries.head(numberOfRows).reset_index()
+    head.rename({"index" : "Smallest_Index", category : "Smallest"}, axis=1, inplace=True)
+    
+    tail = sortedSeries.tail(numberOfRows).reset_index()
+    tail.rename({"index" : "Largest_Index", category : "Largest"}, axis=1, inplace=True)
+
+    # Combine the two along the columns and return the result.
+    return pd.concat([head, tail], axis=1)
+
+
+def ReplaceLowOutlierWithMean(data, category, criteria):
+    """
+    Replaces values beyond the criteria with the mean value for the category.  Done in place.
+
+    Parameters
+    ----------
+    data : Pandas DataFrame
+        The data.
+    category: string
+        Category name in the DataFrame.
+
+    Returns
+    -------
+    None.
+    """
+    mean = data[category].mean()
+    data.loc[data[category] < criteria, category] = mean
+    
+
+def DropMinAndMaxValues(data, category, criteria, method="fraction", inPlace=False):
+    """
+    Drops any rows that do not have data available in the category of "category."
+    
+    Parameters
+    ----------
+    data : pandas.DataFrame
+        DataFrame to change the categories in.
+    category : string
+        Names of the category to look for not available entries.
+    criteria : list of two floats or a float
+        The criteria used to determine which numbers are dropped.  See "method" for more information.
+    method : string
+        How to determine which values are dropped.
+            fraction - A percentage of the top and bottom values are dropped.
+            quantity - The number of values specified is dropped.
+    inPlace : bool
+        If true, the modifications are done in place.
+
+    Returns
+    -------
+    DataFrame without the removed rows or None if inPlace=True.
+    """
+
+    # This will return a struction that has the values and the indices of the minimums and maximums.
+    minAndMaxValues = GetMinAndMaxValues(data, category, criteria, method=method)
+        
+    # numpy.where returns array inside of a tuple for some odd reason.  The [0] extracts the array.
+    dropIndices = pd.concat([minAndMaxValues["Smallest_Index"], minAndMaxValues["Largest_Index"]])
+    
+    # Drop the rows.
+    return data.drop(dropIndices, inplace=inPlace)
+
+    
+def DropOutliers(data, category, irqScale=1.5, inPlace=False):
+    """
+    Drops any rows that are considered outliers by the definition of
+    
+    Parameters
+    ----------
+    data : pandas.DataFrame
+        DataFrame to change the categories in.
+    category : string
+        Names of the category to look for not available entries.
+    irqScale : float
+        Scale factor of interquartile range used to define outliers.
+    inPlace : bool
+        If true, the modifications are done in place.
+
+    Returns
+    -------
+    DataFrame without the removed rows or None if inPlace=True.
+    """
+
+    # Get the stats we need.
+    interQuartileRange = stats.iqr(data[category])
+    limits             = np.quantile(data[category], q=(0.25, 0.75))
+    
+    # Set the outlier limits.
+    limits[0] -= irqScale*interQuartileRange
+    limits[1] += irqScale*interQuartileRange
+    
+    # Gets an DataSeries of boolean values indicating where values are outside of the range.  These are the
+    # values we want to drop.
+    indexMask = (data[category] < limits[0]) | (data[category] > limits[1])
+
+    # The indexMask is a DataSeries that has the indexes from the original DataFrame and the values are the result
+    # of the test statement (bools).  The indices do not necessarily correspond to the location in the DataFrame.  For
+    # example some rows may have been removed.  Extract only the indices we want to remove by using the mask itself.
+    dropIndices = indexMask[indexMask].index
+    
+    # Drop the rows.
+    return data.drop(dropIndices, inplace=inPlace)
+
+
+def RemoveAllUnusedCategories(data):
+    """
+    Removes any unused categories (value count is zero) from all series that are of type "category."
+    Performs operation "in place."
+
+    Parameters
+    ----------
+    data : pandas.DataFrame
+        DataFrame to operate on.
+
+    Returns
+    -------
+    None.
+    """
+    # Find all the category types in the DataFrame and loop over them.
+    for category in data.dtypes[data.dtypes == 'category'].index:
+        data[category] = data[category].cat.remove_unused_categories()
+
+
+def DisplayCategoryCounts(data, categories, useMarkDown=False):
+    """
+    Displays all the values counts for the specified columns columns.
+
+    Parameters
+    ----------
+    data : pandas.DataFrame
+        DataFrame to change the categories in.
+    categories : list, array of strings
+        Names of the categories to operate on.
+
+    Returns
+    -------
+    None.
+    """
+
+    for category in categories:
+        lendres.Console.PrintBoldMessage(category, useMarkDown=useMarkDown)
+        display(data[category].value_counts())
+   
+    
+def DisplayAllCategoriesValueCounts(data, useMarkDown=False):
+    """
+    Displays the value counts of all the DataSeries of type "category."
+
+    Parameters
+    ----------
+    data : pandas.DataFrame
+        DataFrame to operate on.
+
+    Returns
+    -------
+    None.
+    """
+    # Find all the category types in the DataFrame and loop over them.
+    DisplayCategoryCounts(data, data.dtypes[data.dtypes == 'category'].index, useMarkDown)
+
+
+def RemoveRowsWithLowValueCounts(data, column, criteria):
+    """
+    Finds the entries in "column" with low value counts and drops them.
+    Performs operation in place.
+    
+    Parameters
+    ----------
+    data : pandas.DataFrame
+        DataFrame to operate on.
+    column : string
+        Column to search through.
+    criteria : int
+        Values counts less than this value will be dropped.
+
+    Returns
+    -------
+    DataFrame with the low value count rows removed.
+    """
+    # Get the value counts of the column.
+    valueCounts = data[column].value_counts()
+    
+    # Extract the values that are below the threshold criteria.
+    dropValues = valueCounts[valueCounts.values < criteria].index.tolist()
+    
+    # Drop the rows.
+    for value in dropValues:
+         RemoveRowByEntryValue(data, column, value, inPlace=True)
+
+
+def RemoveRowByEntryValue(data, column, value, inPlace=False):
+    """
+    Finds the locations in "column" that are equal to "value" and drops those rows.
+    
+    Parameters
+    ----------
+    data : pandas.DataFrame
+        DataFrame to operate on.
+    column : string
+        Column to search through.
+    value : type of data in column
+        Value to search for and remove.
+    inPlace : bool
+        Specified is the operation should be performed in place.
+
+    Returns
+    -------
+    DataFrame without the removed rows or None if inPlace=True.
+    """
+        
+    # Gets indices of the rows we want to drop.
+    dropIndices = data[data[column] == value].index.tolist()
+    
+    # Drop the rows.
+    return data.drop(dropIndices, inplace=inPlace)
+
+
+def MergeCategories(data, column, fromCategories, toCategory):
+    """
+    Replaces every instance of a value ("from") with another in a column ("to").  Multiple from values can be
+    specified at once.
+    Useful for merging categories of a categorical column.
+    Operation performed in place.
+    
+    Parameters
+    ----------
+    data : pandas.DataFrame
+        DataFrame to operate on.
+
+    Returns
+    -------
+    data : DataFrame
+        The new DataFrame with the encoded values.
+    """
+    for fromCategory in fromCategories:
+        data[column] = data[column].replace({fromCategory : toCategory})
--- lendres/LinearRegressionHelper.py
+++ lendres/LinearRegressionHelper.py
@@ -23,10 +23,51 @@ class LinearRegressionHelper:
     yTrainingData           = []
     yTestData               = []
     regressionModel         = []
-#    data            = []
+    data            = []
 
-#    def __init__(self):
-#        self.data = []
+    def __init__(self):
+        self.data = []
+
+
+    def EncodeAllCategoricalColumns(self, data):
+        """
+        Converts all categorical columns (have that data type "category") to one hot encoded values and drops one
+        value per category.  Prepares categorical columns for use in a model.
+    
+        Parameters
+        ----------
+        data : pandas.DataFrame
+            DataFrame to operate on.
+    
+        Returns
+        -------
+        data : DataFrame
+            The new DataFrame with the encoded values.
+        """
+        # Find all the category types in the DataFrame.
+        # Gets all the columns that have the category data type.  That is returned as a DataSeries.  The
+        # index (where the names are) is extracted from that.
+        allCategoricalColumns = data.dtypes[data.dtypes == 'category'].index.tolist()
+    
+        return self.EncodeCategoricalColumns(data, allCategoricalColumns)
+    
+    
+    def EncodeCategoricalColumns(self, data, categories):
+        """
+        Converts the categorical columns "categories" to one hot encoded values and drops one value per category.
+        Prepares categorical columns for use in a model.
+    
+        Parameters
+        ----------
+        data : pandas.DataFrame
+            DataFrame to operate on.
+    
+        Returns
+        -------
+        data : DataFrame
+            The new DataFrame with the encoded values.
+        """
+        return pd.get_dummies(data, columns=categories, drop_first=True)
 
 
     def CreateLinearModel(self, data, dependentVariable, testSize):
@@ -61,8 +102,7 @@ class LinearRegressionHelper:
         self.regressionModel.fit(self.xTrainingData, self.yTrainingData)
 
 
-
-    def DisplayModelCoefficients(self):
+    def GetModelCoefficients(self):
         """
         Displays the coefficients and intercept of a linear regression model.
 
@@ -72,9 +112,56 @@ class LinearRegressionHelper:
 
         Returns
         -------
-        None.
+        DataFrame that contains the model coefficients.
         """
+        
+        # Make sure the model has been initiated and of the correct type.
+        if not isinstance(self.regressionModel, LinearRegression):
+            raise Exception("The regression model has not be initiated.")
+
         dataFrame = pd.DataFrame(np.append(self.regressionModel.coef_, self.regressionModel.intercept_),
-                                 index = self.xTrainingData.columns.tolist() + ["Intercept"],
-                                 columns = ["Coefficients"])
-        display(dataFrame)
+                                 index=self.xTrainingData.columns.tolist()+["Intercept"],
+                                 columns=["Coefficients"])
+        return dataFrame
+
+        
+    def GetModelPerformanceScores(self):
+        """
+        Calculates the model's scores for the split data (training and testing).
+
+        Parameters
+        ----------
+        None.
+
+        Returns
+        -------
+        DataFrame that contains various performance scores for the training and test data.
+        """
+        # Make sure the model has been initiated and of the correct type.
+        if not isinstance(self.regressionModel, LinearRegression):
+            raise Exception("The regression model has not be initiated.")
+
+        # R squared.
+        trainingScore  = self.regressionModel.score(self.xTrainingData, self.yTrainingData)
+        testScore      = self.regressionModel.score(self.xTestData, self.yTestData)
+        rSquaredScores = [trainingScore, testScore]
+
+        # Mean square error.
+        trainingScore  = mean_squared_error(self.yTrainingData, self.regressionModel.predict(self.xTrainingData))
+        testScore      = mean_squared_error(self.yTestData, self.regressionModel.predict(self.xTestData))
+        mseScores      = [trainingScore, testScore]
+        
+        # Root mean square error.
+        rmseScores     = [np.sqrt(trainingScore), np.sqrt(testScore)]
+        
+        # Mean absolute error.
+        trainingScore  = mean_absolute_error(self.yTrainingData, self.regressionModel.predict(self.xTrainingData))
+        testScore      = mean_absolute_error(self.yTestData, self.regressionModel.predict(self.xTestData))
+        maeScores      = [trainingScore, testScore]
+
+        dataFrame      = pd.DataFrame({"R Squared" : rSquaredScores,
+                                       "RMSE"      : rmseScores,
+                                       "MSE"       : mseScores,
+                                       "MAE"       : maeScores},
+                                       index=["Training", "Test"])
+        return dataFrame
