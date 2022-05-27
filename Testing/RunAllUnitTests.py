ec7b7b98f8b01901c59e510e5c2308d82a15848a
Ikosy <lendres@fifthrace.com>
2022-05-27 12:18:41 -0500

    Updates for running unit tests.

--- Testing/RunAllUnitTests.py
+++ Testing/RunAllUnitTests.py
@@ -5,15 +5,9 @@ Created on April 19, 2022.
 
 import unittest
 
-
 def RunAllTests(startDirectory):
     loader   = unittest.TestLoader()
     suite    = loader.discover(startDirectory)
 
     runner   = unittest.TextTestRunner()
     runner.run(suite)
-
-
-# Default for running from the hard drive.
-startDirectory = "C:\\Custom Program Files\\Python\\Testing"
-RunAllTests(startDirectory)
---
+++ Testing/RunAllUnitTestsScript.py
@@ -0,0 +1,10 @@
+"""
+Created on May 27, 2022
+@author: Lance
+"""
+
+from Testing.RunAllUnitTests import RunAllTests
+
+# Default for running from the hard drive.
+startDirectory = "C:\\Custom Program Files\\Python\\Testing"
+RunAllTests(startDirectory)
