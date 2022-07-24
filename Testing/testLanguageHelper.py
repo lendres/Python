2198fb9f56c8ec5298bad462bb2a0c4f2933cf67
Lance A. Endres <lendres@fifthrace.com>
2022-07-24 17:51:43 -0500

    Natural language processing updates
    Updates to operate on different data structures.
    Ability to remove website addresses.
    Documentation updates.

--- Testing/testLanguageHelper.py
+++ Testing/testLanguageHelper.py
@@ -25,15 +25,27 @@ class TestNLPHelper(unittest.TestCase):
         """
         Set up function that runs before each test.
         """
-        self.testList = ["The", ",", "and", ",", "if", "are", "stopwords", ",", "computer", "is", "not"]
+        self.testList = ["The", ",", "and", "if", "are", "stopwords", "computer", "is", "not"]
         self.tweets   = TestNLPHelper.tweets.copy(deep=True)
 
 
     def testStopWords(self):
-        LanguageHelper.RemoveStopWords("no")
-        LanguageHelper.RemoveStopWords("not")
-        result    = LanguageHelper.FilterOutStopWords(self.testList)
-        self.assertEqual(result, ["The", ",", ",", "stopwords", ",", "computer", "not"])
+        LanguageHelper.ResetStopWordsList()
+        result    = LanguageHelper.RemoveStopWords(self.testList)
+        self.assertEqual(result, ["The", ",", "stopwords", "computer"])
+
+        LanguageHelper.RemoveFromStopWordsList("no")
+        LanguageHelper.RemoveFromStopWordsList("not")
+        result    = LanguageHelper.RemoveStopWords(self.testList)
+        self.assertEqual(result, ["The", ",", "stopwords", "computer", "not"])
+
+        LanguageHelper.AppendToStopWordsList("computer")
+        result    = LanguageHelper.RemoveStopWords(self.testList)
+        self.assertEqual(result, ["The", ",", "stopwords", "not"])
+
+        result    = LanguageHelper.RemoveStopWords("The, and if are stopwords computer is not")
+        self.assertEqual(result, "The, stopwords not")
+
 
 
     def testStripHtml(self):
@@ -47,7 +59,7 @@ class TestNLPHelper(unittest.TestCase):
 
 
     def testTokenize(self):
-        result   = LanguageHelper.Tokenize("The , and , if are stopwords, computer is not")
+        result   = LanguageHelper.Tokenize("The , and if are stopwords computer is not")
         solution = self.testList
         self.assertEqual(result, solution)
 
@@ -77,20 +89,44 @@ class TestNLPHelper(unittest.TestCase):
         self.assertEqual(result, ["title", "case", "string"])
 
 
-    def testStripHandles(self):
+    def testRemoveInternetHandles(self):
         # Test on a Pandas.Series.
-        result = LanguageHelper.StripHandles(self.tweets["text"])
+        result = LanguageHelper.RemoveInternetHandles(self.tweets["text"])
         self.assertEqual(result.loc[0].strip(), "What said.")
 
         # Test on a string.
-        result = LanguageHelper.StripHandles(self.tweets["text"].loc[0])
+        result = LanguageHelper.RemoveInternetHandles(self.tweets["text"].loc[0])
         self.assertEqual(result.strip(), "What said.")
 
         # Test on a list.
         inputList = [self.tweets["text"].loc[0], self.tweets["text"].loc[1]]
-        result = LanguageHelper.StripHandles(inputList)
+        result = LanguageHelper.RemoveInternetHandles(inputList)
         self.assertEqual(result[0].strip(), "What said.")
 
+
+    def testRemoveWebsiteAddresses(self):
+        text = self.tweets["text"].loc[21]
+        result = LanguageHelper.RemoveWebAddresses(text)
+        print("\n\n\n", text)
+        print("\n", result)
+
+        text = self.tweets["text"].loc[32]
+        result = LanguageHelper.RemoveWebAddresses(text)
+        print("\n\n\n", text)
+        print("\n", result)
+
+        text = self.tweets["text"].loc[59]
+        result = LanguageHelper.RemoveWebAddresses(text)
+        print("\n\n\n", text)
+        print("\n", result)
+
+
+    def testWordCloud(self):
+        LanguageHelper.ResetStopWordsList()
+        LanguageHelper.CreateWordCloud(self.tweets["text"])
+
+        LanguageHelper.AppendToStopWordsList(["thank", "thanks", "plane", "flight", "flights", "im", "u"])
+        LanguageHelper.CreateWordCloud(self.tweets["text"], removeStopWords=True)
 
 
 if __name__ == "__main__":
--- lendres/LanguageHelper.py
+++ lendres/LanguageHelper.py
@@ -11,7 +11,12 @@ from   bs4                                       import BeautifulSoup
 import nltk
 from   nltk.tokenize.toktok                      import ToktokTokenizer
 import contractions
-import wordcloud
+
+#for plotting images & adjusting colors
+import matplotlib.pyplot                         as plt
+from   wordcloud                                 import WordCloud
+from   wordcloud                                 import STOPWORDS
+from   wordcloud                                 import ImageColorGenerator
 
 import re
 import spacy
@@ -31,7 +36,19 @@ class LanguageHelper():
 
 
     @classmethod
-    def RemoveStopWords(cls, words):
+    def RemoveFromStopWordsList(cls, words):
+        """
+        Remove words from the list of stop words.
+
+        Parameters
+        ----------
+        words : string or list of strings
+            Words to remove from the stop words.
+
+        Returns
+        -------
+        None.
+        """
         if type(words) != list:
             words = [words]
 
@@ -42,8 +59,68 @@ class LanguageHelper():
 
 
     @classmethod
-    def FilterOutStopWords(cls, tokens):
-        return [token for token in tokens if token not in cls.stopWords]
+    def AppendToStopWordsList(cls, words):
+        """
+        Add words to the list of stop words.
+
+        Parameters
+        ----------
+        words : string or list of strings
+            Words to remove from the stop words.
+
+        Returns
+        -------
+        None.
+        """
+        if type(words) != list:
+            words = [words]
+
+        for word in words:
+            # Prevent an error or trying to remove a word not in the list.
+            if word not in cls.stopWords:
+                cls.stopWords.append(word)
+
+
+    @classmethod
+    def ResetStopWordsList(cls):
+        """
+        Resets the stop words list.
+
+        Parameters
+        ----------
+        None.
+
+        Returns
+        -------
+        None.
+        """
+        cls.stopWords = nltk.corpus.stopwords.words("english")
+
+
+    @classmethod
+    def RemoveStopWords(cls, text):
+        result  = None
+
+        if type(text) == pd.core.series.Series:
+            result = text.apply(lambda entry : cls.RemoveStopWordsFromString(entry))
+        elif type(text) == list:
+            result = [token for token in text if token not in cls.stopWords]
+        elif type(text) == str:
+            result = cls.RemoveStopWordsFromString(text)
+
+        return result
+
+
+    @classmethod
+    def RemoveStopWordsFromString(cls, text):
+        result = text
+
+        for word in cls.stopWords:
+            pattern = r"(^|[^\w])" + word + r"\b"
+            #pattern = r"(?:^|\W)rocket(?:$|\W)"
+            result = re.sub(pattern, "", result)
+
+        return result
 
 
     @classmethod
@@ -69,31 +146,85 @@ class LanguageHelper():
     @classmethod
     def Tokenize(cls, text):
         tokenizer = ToktokTokenizer()
-        tokens    = tokenizer.tokenize(text)
-        return [token.strip() for token in tokens]
+
+        result  = None
+
+        if type(text) == pd.core.series.Series:
+            result = text.apply(lambda entry : tokenizer.tokenize(entry))
+            result = result.apply(lambda tokens : [token.strip() for token in tokens])
+        elif type(text) == list:
+            result = []
+            for i in range(len(text)):
+                tokens = tokenizer.tokenize(text[i])
+                result.append([token.strip() for token in tokens])
+        elif type(text) == str:
+            tokens = tokenizer.tokenize(text)
+            result = [token.strip() for token in tokens]
+
+        return result
 
 
     @classmethod
     def RemoveSpecialCharacters(cls, text, removeDigits=False):
         """
-        Removes special characters.
+        Removes special characters.  This function automatically operates
+        on the text in the correct way for different types of data structions.
+
+        Parameters
+        ----------
+        text : Pandas DataFrame, list, or string
+            The text to operate on.
+        pattern : string
+            regular expression to operate on.
+
+        Returns
+        -------
+        text : Pandas DataFrame, list, or string
+            The processed text.
         """
         pattern = r"[^a-zA-z\s]" if removeDigits else  r"[^a-zA-z0-9\s]"
         return LanguageHelper.ApplyRegularExpression(text, pattern)
 
 
+    @classmethod
     def RemovePunctuation(cls, text):
         """
-        Removes punctuation.
+        Removes punctuation.  This function automatically operates
+        on the text in the correct way for different types of data structions.
+
+        Parameters
+        ----------
+        text : Pandas DataFrame, list, or string
+            The text to operate on.
+        pattern : string
+            regular expression to operate on.
+
+        Returns
+        -------
+        text : Pandas DataFrame, list, or string
+            The processed text.
         """
         pattern = r"[^\w\s]"
         return LanguageHelper.ApplyRegularExpression(text, pattern)
 
 
     @classmethod
-    def StripHandles(cls, text):
+    def RemoveInternetHandles(cls, text):
         """
-        Removes Twitter handles.
+        Removes Twitter handles.  This function automatically operates
+        on the text in the correct way for different types of data structions.
+
+        Parameters
+        ----------
+        text : Pandas DataFrame, list, or string
+            The text to operate on.
+        pattern : string
+            regular expression to operate on.
+
+        Returns
+        -------
+        text : Pandas DataFrame, list, or string
+            The processed text.
         """
         # @(\w{1,15})
         # Matches the @ followed by characters.
@@ -102,6 +233,45 @@ class LanguageHelper():
         # Removes extraneous spaces from around the match.
         pattern = r"(^|[^@\w])@(\w{1,15})\b"
         return LanguageHelper.ApplyRegularExpression(text, pattern)
+
+
+    @classmethod
+    def RemoveWebAddresses(cls, text):
+        pattern = r"(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{0,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)"
+        return LanguageHelper.ApplyRegularExpression(text, pattern)
+
+
+    @classmethod
+    def CreateWordCloud(cls, text, width=800, height=600, removeStopWords=False):
+        text = LanguageHelper.RemoveInternetHandles(text)
+        text = LanguageHelper.RemoveWebAddresses(text)
+        text = LanguageHelper.ToLowerCase(text)
+
+        if removeStopWords:
+            text = LanguageHelper.RemoveStopWords(text)
+
+        #text = LanguageHelper.RemovePunctuation(text)
+        #text = LanguageHelper.RemoveSpecialCharacters(text)
+        #text = LanguageHelper.StripHtmlTags(text)
+
+        if type(text) != list:
+            text = text.tolist()
+
+        text = " ".join(text)
+
+        wordcloud = WordCloud(
+            stopwords=STOPWORDS,
+            background_color="white",
+            colormap="viridis",
+            width=width,
+            height=height,
+            collocations=True
+        ).generate(text)
+
+        # Plot the wordcloud object.
+        plt.imshow(wordcloud, interpolation="bilInear")
+        plt.axis("off")
+        plt.show()
 
 
     @classmethod
@@ -139,8 +309,20 @@ class LanguageHelper():
     @classmethod
     def ToLowerCase(cls, text):
         """
-        Convert all characters to lowercase.
-        If the input is a list, the operation is performed in place.
+        Convert all characters to lowercase.  This function automatically operates
+        on the text in the correct way for different types of data structions.
+
+        Parameters
+        ----------
+        text : Pandas DataFrame, list, or string
+            The text to operate on.
+        pattern : string
+            regular expression to operate on.
+
+        Returns
+        -------
+        text : Pandas DataFrame, list, or string
+            The processed text.
         """
         result = None
 
@@ -166,6 +348,9 @@ class LanguageHelper():
 
     @classmethod
     def SimpleStemmer(cls, text):
+        """
+        Stemming using Porter Stemmer.
+        """
         porterStemmer = nltk.porter.PorterStemmer()
         text          = " ".join([porterStemmer.stem(word) for word in text.split()])
         return text
@@ -178,6 +363,6 @@ class LanguageHelper():
         # Install language packages using Anaconda environments.
         nlp  = spacy.load("en_core_web_sm")
         text = nlp(text)
-        text = " ".join([word.lemma_ if word.lemma_ != "-PRON-" else word.text for word in text])
-        #text = " ".join([word.text if word.lemma_ == "-PRON-" else word.lemma_ for word in text])
+        #text = " ".join([word.lemma_ if word.lemma_ != "-PRON-" else word.text for word in text])
+        text = " ".join([word.text if word.lemma_ == "-PRON-" else word.lemma_ for word in text])
         return text
