4a37007d2fec6a6ec835aa4880d692618f1e35fe
Ikosy <lendres@fifthrace.com>
2022-08-13 18:16:50 -0500

    Added properties and equality overrides.

--- lendres/Geometry/Point.py
+++ lendres/Geometry/Point.py
@@ -30,45 +30,84 @@ class Point(Shape):
             self.values = np.array(values)
 
 
+    @property
+    def X(self):
+        return self.values[0]
+
+
+    @property
+    def Y(self):
+        return self.values[1]
+
+
+    @property
+    def Z(self):
+        if len(self.values) < 3:
+            raise Exception("Point is not contain three dimensions.")
+        return self.values[2]
+
+
     def __add__(self, obj):
         # Adding two objects.
-        size   = len(self.values)
-        values = [0] * size
-
+        size       = len(self.values)
+        values     = [0] * size
         objectType = type(obj)
 
         if objectType == Point:
             for i in range(size):
                 values[i] = self.values[i] + obj.values[i]
 
-        if objectType == int or objectType == float:
+        elif objectType == list or objectType == np.ndarray:
+            for i in range(size):
+                values[i] = self.values[i] + obj[i]
+
+        elif objectType == int or objectType == float:
             for i in range(size):
                 values[i] = self.values[i] + obj
+
+        else:
+            raise Exception("Object type not found.  Object type:" + str(objectType))
 
         return Point(values)
 
 
-    def EqualsPoint(self, point):
-        size = len(self.values)
+    def __eq__(self, obj):
+        # Adding two objects.
+        size       = len(self.values)
+        objectType = type(obj)
 
-        if size != len(point.values):
+        if objectType == Point:
+            for i in range(size):
+                if not Precision.Equal(self.values[i], obj.values[i]):
+                    return False
+            return True
+
+        elif objectType == list or objectType == np.ndarray:
+            for i in range(size):
+                if not Precision.Equal(self.values[i], obj[i]):
+                    return False
+            return True
+
+        else:
+            raise Exception("Object type not found.  Object type:" + str(objectType))
+
+
+    def __ne__(self, obj):
+        # Adding two objects.
+        size       = len(self.values)
+        objectType = type(obj)
+
+        if objectType == Point:
+            for i in range(size):
+                if not Precision.Equal(self.values[i], obj.values[i]):
+                    return True
             return False
 
-        for i in range(size):
-            equal = Precision.Equal(self.values[i], point.values[i])
-            if not equal:
-                return False
-        return True
-
-
-    def EqualsValues(self, values):
-        size = len(self.values)
-
-        if size != len(values):
+        elif objectType == list or objectType == np.ndarray:
+            for i in range(size):
+                if not Precision.Equal(self.values[i], obj[i]):
+                    return True
             return False
 
-        for i in range(size):
-            equal = Precision.Equal(self.values[i], values[i])
-            if not equal:
-                return False
-        return True
+        else:
+            raise Exception("Object type not found.  Object type:" + str(objectType))
