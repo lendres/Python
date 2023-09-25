9ae25fbcf08a579aad810c3764366bcf541f0524
lendres <lanceendres@gmail.com>
2023-09-25 11:28:21 -0500

    Count nested objects to get total number of elements

--- lendres/algorithms/DataType.py
+++ lendres/algorithms/DataType.py
@@ -3,6 +3,7 @@ Created on July 24, 2023
 @author: Lance A. Endres
 """
 
+
 class DataType():
     """
     A class for checking and determining data types.  These are advanced checks, for example,
@@ -24,7 +25,7 @@ class DataType():
         : boolean
             Returns true if ALL the elements of the list are lists.
         """
-        return all(isinstance(element, list) for element in inputList)
+        return all(isinstance(element, list) | isinstance(element, tuple) for element in inputList)
 
 
     @classmethod
@@ -42,11 +43,27 @@ class DataType():
         : boolean
             Returns true if ANY the elements of the list are lists.
         """
-        return any(isinstance(element, list) for element in inputList)
+        return any(isinstance(element, list) | isinstance(element, tuple) for element in inputList)
 
 
     @classmethod
-    def AreListsOfListsSameSize(cls, listOfLists1, listOfLists2):
+    def AreListsOfListsSameSize(cls, listOfLists1:list|tuple, listOfLists2:list|tuple):
+        """
+        Determines if a set of nested object are the same size.  To be the same size, each sub list/tuple/et cetera must be the same size
+
+        Parameters
+        ----------
+        listOfLists1 : list|tuple
+            First nested object.
+        listOfLists2 : list|tuple
+            Second nested object.
+
+
+        Returns
+        -------
+        : bool
+            True if every element and sub element are the same length, False otherwise.
+        """
         if not (cls.IsListOfLists(listOfLists1) and cls.IsListOfLists(listOfLists2)):
             raise Exception("At least one of the inputs is not a list of lists.")
 
@@ -68,3 +85,57 @@ class DataType():
 
         result = [[initializationValue for size in sizeList] for sizeList in sizes]
         return result
+
+
+    @classmethod
+    def GetLengthOfNestedObjects(cls, nestedObjects:list|tuple):
+        """
+        Counts the total number of objects is a list/tuple/et cetera.  Recursively counts the elements.
+        Example:
+            GetLengthOfNestedObjects([1, [2, 3]])
+            result: 3
+
+        Parameters
+        ----------
+        nestedObjects : list|tuple
+            An iterable object.
+        Returns
+        -------
+        : int
+            Total number of elements in the object.
+        """
+        return cls._CountNestedObjects(0, nestedObjects)
+
+
+    @classmethod
+    def _CountNestedObjects(cls, count:int, obj:int|float|str|list|tuple):
+        """
+        Drills down iterable objects counting each element as it goes.
+        Counts an element if it is not iterable (e.g. an int) or iteratotes over each element if it is iterable.
+
+        Parameters
+        ----------
+        count : int
+            The current count of individual objects.
+        obj : int|list|tuple
+            Current object of interest.
+
+        Returns
+        -------
+        count : int
+            The current count of found individual elements.
+        """
+        match obj:
+            # For single elements we add to the count.
+            case int() | float() | str():
+                return count + 1
+
+            # For iterable objects we loop over them and call ourself to continue to interigation.
+            case list() | tuple():
+                for item in obj:
+                    count = cls._CountNestedObjects(count, item)
+                return count
+
+            # Catch any data types that have not been acounted for and raise an error.
+            case _:
+                raise Exception("Unknown object type.")
