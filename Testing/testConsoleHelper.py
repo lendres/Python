70a8f1ccaa232d44e1e5877cb3c6c0291b1cfaf1
Lance A. Endres <lanceendres@gmail.com>
2023-10-05 11:02:26 -0500

    Create testConsoleHelper.py

---
+++ Testing/testConsoleHelper.py
@@ -0,0 +1,36 @@
+"""
+Created on December 27, 2021
+@author: Lance A. Endres
+"""
+from   lendres.ConsoleHelper                                    import ConsoleHelper
+
+import unittest
+
+
+skipTests = False
+
+class TestDataHelper(unittest.TestCase):
+    #verboseLevel = ConsoleHelper.VERBOSENONE
+    #verboseLevel = ConsoleHelper.VERBOSETESTING
+    #verboseLevel = ConsoleHelper.VERBOSEREQUESTED
+    #verboseLevel = ConsoleHelper.VERBOSEIMPORTANT
+    verboseLevel = ConsoleHelper.VERBOSEALL
+
+    @classmethod
+    def setUpClass(cls):
+
+        cls.consoleHelper = ConsoleHelper(verboseLevel=cls.verboseLevel)
+
+
+    def setUp(self):
+        pass
+
+
+    def testPrintInColor(self):
+        self.consoleHelper.PrintInColor("This is a test of foreground color.", (0, 255, 0))
+        self.consoleHelper.PrintInColor("This is a test of foreground color and background.", (255, 0, 0), (255, 255, 255))
+        self.consoleHelper.Print("Standard print, did the color return to normal?")
+
+
+if __name__ == "__main__":
+    unittest.main()
