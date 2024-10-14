9de8b16306fd9513d4b54e5e1e01b607c6edc9bd
Lance A. Endres <lendres@fifthrace.com>
2024-10-14 05:26:28 -0500

    Update FunctionGenerator.py

--- lendres/demonstration/FunctionGenerator.py
+++ lendres/demonstration/FunctionGenerator.py
@@ -91,7 +91,7 @@ class FunctionGenerator():
         x, y   = cls.SineWave(magnitude, frequency, yOffset, slope, startTime, timeLength, steps)
 
         noise   = noiseScale*magnitude*np.random.randn(len(x))
-        yNoisey = y + noise
+        yNoisy = y + noise
 
         return x, yNoisey, y, noise
 
