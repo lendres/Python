e85d3c89d2d73aaa0ff5b5bb4d17e74580364df0
Allan Gurwicz <45400615+agurwicz@users.noreply.github.com>
2025-07-23 11:06:03 -0500

    Generalization of vector norms

--- lendres/mathematics/LinearAlgebra.py
+++ lendres/mathematics/LinearAlgebra.py
@@ -2,15 +2,16 @@
 Created on July 13, 2025
 @author: Lance A. Endres
 """
+import re
 import numpy                       as np
 from   lendres.datatypes.ListTools import ListTools
-from typing                        import Union
+from   typing                      import Union
 
 
 class LinearAlgebra():
 
     @classmethod
-    def Normalize(cls, vector:list|tuple|np.ndarray, norm:str="L2", returnNorm:bool=False) -> Union[np.ndarray, tuple[np.ndarray, float]]:
+    def Normalize(cls, vector:list|tuple|np.ndarray, norm:int|float|str = 2, returnNorm:bool=False) -> Union[np.ndarray, tuple[np.ndarray, float]]:
         """
         Normalizes a 1 dimensional vector.
 
@@ -18,8 +19,9 @@ class LinearAlgebra():
         ----------
         vector : list|tuple|np.ndarray
             The vector to normalize.
-        norm : str, optional
-            Normalization method.  Accepted  The default is "L2".
+        norm : int|float|str, optional
+            Order of the norm. The default is 2 for the L2 norm.
+            For the infinity norm, pass "max", "inf" or np.inf.
         returnNorm : bool, optional
             If True, the norm will be returned as a second value. The default is False.
 
@@ -42,16 +44,22 @@ class LinearAlgebra():
                 if vector.ndim > 1:
                     raise Exception("This method is for one dimensional arrays only.")
 
-        normalizingValue = 0
-        match norm:
-            case "L1":
-                normalizingValue = np.sum(np.abs(vector))
-            case "L2":
-                normalizingValue = np.linalg.norm(vector)
-            case "max":
-                normalizingValue = np.max(np.abs(vector))
-            case _:
+        if norm in ["max", "inf", np.inf]:
+            norm = np.inf
+
+        elif isinstance(norm, str):
+
+            # Transforming "L1" into 1, "l2" into 2, etc.
+            match = re.match(pattern=r"^L([0-9]){1}$", string=norm, flags=re.IGNORECASE)
+            if match is not None:
+                norm = match.groups()[0]
+
+            try:
+                norm = int(norm)
+            except ValueError:
                 raise Exception("Invalid norm type provided.")
+
+        normalizingValue = np.linalg.norm(x=vector, ord=norm)
 
         if returnNorm:
             return vector / normalizingValue, normalizingValue
